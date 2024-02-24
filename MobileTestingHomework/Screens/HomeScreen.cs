0af0d3c241773be0c46d439d70b84b1b886c3a62
Alexey Sirko <leshasirko@gmail.com>
2024-02-24 04:33:26 +0300

    added project to the branch

---
+++ MobileTestingHomework/Screens/HomeScreen.cs
@@ -0,0 +1,15 @@
+﻿using Aquality.Appium.Mobile.Screens;
+
+namespace MobileTestingHomework.Screens
+{
+    internal class HomeScreen : Screen
+    {
+        private readonly static By s_homeLabelXpath = By.XPath("//android.widget.TextView[@text='Home']");
+
+        public HomeScreen() : base(s_homeLabelXpath, "Home")
+        {
+        }
+
+
+    }
+}
---
+++ MobileTestingHomework/StepDefinitions/TabBarStepDefinitions.cs
@@ -0,0 +1,16 @@
+﻿using MobileTestingHomework.Screens.SearchTab;
+
+namespace MobileTestingHomework.StepDefinitions
+{
+    [Binding]
+    internal class TabBarStepDefinitions
+    {
+        private readonly TabBar _tabBar = new();
+
+        [When(@"Tap '(.*)' tab")]
+        public void SwitchTab(string tabName)
+        {
+            _tabBar.ClickTabButtonByIdPart(tabName);
+        }
+    }
+}
